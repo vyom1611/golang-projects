31af4d2d10d5f118b417c14e278bbcd30d6eac9d
vyom <sharm843@umn.edu>
2022-07-11 22:28:40 +0530

    Web server developed

--- Basic web-server/go-server/main.go
+++ Basic web-server/go-server/main.go
@@ -1,7 +1,43 @@
 package main
 
-import("fmt", "log", "net/http")
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() error: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Post request succesfull")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Printf("Name: %v, Address: %v", name, address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Fprint(w, "Hello, World!")
+}
 
 func main() {
-	
+	var fileServer = http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Server started on port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
