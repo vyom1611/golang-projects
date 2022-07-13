306044a7192e533a411b14b719541ee5f2fce8db
vyom <sharm843@umn.edu>
2022-07-13 19:48:44 +0530

    Simple email checker

--- Email verifier tool/main.go
+++ Email verifier tool/main.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"os"
+	"strings"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Printf("domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord\n")
+	fmt.Printf("domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord\n")
 
 	for scanner.Scan() {
 		checkDomain(scanner.Text())
@@ -21,5 +23,42 @@ func main() {
 }
 
 func checkDomain(domain string) {
+	var hasMX, hasSPF, hasDMARC bool
+	var spfRecord, dmarcRecord string
 
+	mxRecords, err := net.LookupMX(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+
+
+	txtRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
+
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Print("%v %v %v %v %v %v", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
 }
